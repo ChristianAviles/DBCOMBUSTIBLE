473974decca0553d09885889dec9ff8ba7285c5c
ChristianAviles <caviles1604@gmail.com>
2019-09-12 16:58:03 -0600

    COMMIT DESDE LAPTOP PERSONAL

---
+++ VersionSQL/CHRISTIANAVILES/SQLENTERPRISE/BDCOMBUSTIBLE/dbo/Procedure/mostrar_categoriaVehiculo.sql
@@ -0,0 +1,5 @@
+/****** Object:  Procedure [dbo].[mostrar_categoriaVehiculo]    Committed by VersionSQL https://www.versionsql.com ******/
+
+create proc mostrar_categoriaVehiculo
+as
+select * from vCategoriaVehiculo
